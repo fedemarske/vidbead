fdd044c2173970f4835f01048535bc4bace74a13
unknown <psoto@PC99.springfield.mobilecomputing.com.ar>
2015-07-20 16:47:19 -0300

    commit 1

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vidbeadApp
+ * @description
+ * # vidbeadApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vidbeadApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
